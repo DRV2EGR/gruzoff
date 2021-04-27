c43fac7ddc8a2e27447a2895c15b6b58b6594f09
DRV2VNA <1664286@gmail.com>
2021-04-27 16:07:44 +0300

    Project created

---
+++ src/main/java/ru/gruzoff/GruzoffApplication.java
@@ -0,0 +1,13 @@
+package ru.gruzoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruzoffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GruzoffApplication.class, args);
+    }
+
+}
