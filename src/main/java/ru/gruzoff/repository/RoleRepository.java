d6a821e33d43732622c2506dd57fccf42a498bfe
DRV2VNA <1664286@gmail.com>
2021-05-08 16:28:08 +0300

    Добавлен RefreshToken

--- src/main/java/ru/gruzoff/repository/RoleRepository.java
+++ src/main/java/ru/gruzoff/repository/RoleRepository.java
@@ -3,5 +3,5 @@ package ru.gruzoff.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.gruzoff.entity.Role;
 
-public class RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
