4e4a2e976d37305ff73aed58616b07909d15143f
DRV2VNA <1664286@gmail.com>
2021-05-08 16:37:10 +0300

    Добавлены исключения для сервиса.

---
+++ src/main/java/ru/gruzoff/exception/ConflictException.java
@@ -0,0 +1,13 @@
+package ru.gruzoff.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * The type Conflict exception.
+ */
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ConflictException extends ApiClientException {
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/ru/gruzoff/exception/UserNotFoundExeption.java
@@ -0,0 +1,11 @@
+package ru.gruzoff.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * The type User not found exeption.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundExeption extends ApiServerException{
+}
