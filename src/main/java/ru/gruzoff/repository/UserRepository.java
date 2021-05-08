2778c4049e5a336e263e3d9bc05ab910fc130cf2
DRV2VNA <1664286@gmail.com>
2021-05-08 16:21:27 +0300

    Добавлены UserDtoPayload, BasicPayload, новые методы UserRepository

--- src/main/java/ru/gruzoff/repository/UserRepository.java
+++ src/main/java/ru/gruzoff/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package ru.gruzoff.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.gruzoff.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
