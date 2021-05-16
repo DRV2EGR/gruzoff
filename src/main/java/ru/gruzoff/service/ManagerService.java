46f834cbe2927b38c2fe82c9bf1d848dce3872da
DRV2VNA <1664286@gmail.com>
2021-05-16 22:55:32 +0300

    Доработка логирования.

--- src/main/java/ru/gruzoff/service/ManagerService.java
+++ src/main/java/ru/gruzoff/service/ManagerService.java
@@ -1,9 +1,13 @@
 package ru.gruzoff.service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.gruzoff.dto.OrderDto;
@@ -12,12 +16,18 @@ import ru.gruzoff.exception.NotFoundException;
 import ru.gruzoff.repository.OrderReposiory;
 
 @Service
+@Slf4j
 public class ManagerService {
     @Autowired
     OrderReposiory orderReposiory;
 
     @Autowired
     ClassToDtoService classToDtoService;
+
+    @Autowired
+    MailService mailService;
+
+    Logger logger = LoggerFactory.getLogger("orderLogger");
 
     public List<OrderDto> getOrdersToAccept() {
         List<OrderDto> orderDtoList = new ArrayList<>();
@@ -37,6 +47,10 @@ public class ManagerService {
                     () -> new NotFoundException("Order not found")
             );
             order.setStatus("IN_WORK");
+
+            logger.info("Order " + order.getId() + " approved at " + LocalDateTime.now());
+            mailService.send(order.getCustomerId().getUser().getEmail(), "Заказ подтвержден.",
+                    mailService.creationOrderNotify(order.getCustomerId().getUser(), order));
             orderReposiory.save(order);
         } else if (st == 2) { // UserNotAccept
             Order order = orderReposiory.findById(orderId).orElseThrow(
