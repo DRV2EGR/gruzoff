22885863f587bf8036794e1140482107a57960e9
Dmitry <1664286@gmail.com>
2021-05-23 18:25:38 +0300

    FIX: mail config

--- src/main/java/ru/gruzoff/config/MailConfig.java
+++ src/main/java/ru/gruzoff/config/MailConfig.java
@@ -13,44 +13,44 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
  */
 @Configuration
 public class MailConfig {
-    @Value("${spring.mail.host}")
-    private String host;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Value("${spring.mail.password}")
-    private String password;
-
-    @Value("${spring.mail.port}")
-    private int port;
-
-    @Value("${spring.mail.protocol}")
-    private String protocol;
-
-    @Value("${mail.debug}")
-    private String debug;
-
-    /**
-     * Gets mail sender.
-     *
-     * @return the mail sender
-     */
-    @Bean
-    public JavaMailSender getMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-        mailSender.setHost(host);
-        mailSender.setPort(465);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
-
-        Properties properties = mailSender.getJavaMailProperties();
-
-        properties.setProperty("mail.transport.protocol", protocol);
-        properties.setProperty("mail.debug", debug);
-
-
-        return mailSender;
-    }
+//    @Value("${spring.mail.host}")
+//    private String host;
+//
+//    @Value("${spring.mail.username}")
+//    private String username;
+//
+//    @Value("${spring.mail.password}")
+//    private String password;
+//
+//    @Value("${spring.mail.port}")
+//    private int port;
+//
+//    @Value("${spring.mail.protocol}")
+//    private String protocol;
+//
+//    @Value("${mail.debug}")
+//    private String debug;
+//
+//    /**
+//     * Gets mail sender.
+//     *
+//     * @return the mail sender
+//     */
+//    @Bean
+//    public JavaMailSender getMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//
+//        mailSender.setHost(host);
+//        mailSender.setPort(465);
+//        mailSender.setUsername(username);
+//        mailSender.setPassword(password);
+//
+//        Properties properties = mailSender.getJavaMailProperties();
+//
+//        properties.setProperty("mail.transport.protocol", protocol);
+//        properties.setProperty("mail.debug", debug);
+//
+//
+//        return mailSender;
+//    }
 }
